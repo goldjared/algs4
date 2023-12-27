2c6385179290a09cad52b175a6d8f24bb6aefb80
jared <jaredgold@proton.me>
2023-12-26 22:20:47 -0500

    complete Deque to spec

--- queues/src/Deque.java
+++ queues/src/Deque.java
@@ -6,6 +6,9 @@
  
  */
 
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class Deque<Item> implements Iterable<Item> {
 	Node head;
 	Node tail;
@@ -29,26 +32,23 @@ public class Deque<Item> implements Iterable<Item> {
 	}
 	
 	// return the number of items on the deque
-	public int size() {
+	public int getSize() {
 		return size;
 	}
 	
 	// add the item to the front
 	public void addFirst(Item item) {
-		// Throw an IllegalArgumentException if the client calls either
-		// addFirst() or addLast() with a null argument.
+		if(item == null) throw new IllegalArgumentException();
 		Node oldHead = head;
 		head = new Node();
 		head.item = item;
 		head.next = oldHead;
 		size++;
-		
 	}
 	
 	// add the item to the back
 	public void addLast(Item item) {
-		// Throw an IllegalArgumentException if the client calls either
-		// addFirst() or addLast() with a null argument.
+		if(item == null) throw new IllegalArgumentException();
 		Node newTail = new Node();
 		newTail.item = item;
 		newTail.next = null;
@@ -58,8 +58,7 @@ public class Deque<Item> implements Iterable<Item> {
 	
 	// remove and return the item from the front
 	public Item removeFirst() {
-		//	 Throw a java.util.NoSuchElementException if the client calls either
-		//	 removeFirst() or removeLast when the deque is empty.
+		if(isEmpty()) throw new NoSuchElementException();
 		Item removedHead = head.item;
 		head = head.next;
 		size--;
@@ -68,17 +67,43 @@ public class Deque<Item> implements Iterable<Item> {
 	
 	// remove and return the item from the back
 	public Item removeLast() {
-		//	 Throw a java.util.NoSuchElementException if the client calls either
-		//	 removeFirst() or removeLast when the deque is empty.
+		if(isEmpty()) throw new NoSuchElementException();
+		Iterator<Item> removalIterate = iterator();
+		for(int i = 0; i<size-1; i++) {
+			removalIterate.next();
+		}
+		Node lastVisited = ((ListIterator)	removalIterate).getLastVisited();
+		lastVisited.next = null;
+	  size--;
+		return lastVisited.item;
 	}
 	
 	// return an iterator over items in order from front to back
 	public Iterator<Item> iterator() {
+		return new ListIterator();
+	}
 	
+	private class ListIterator implements Iterator<Item> {
+		private Node current = head;
+		private Node lastVisited = null;
+		public boolean hasNext() {
+			return current != null;
+		}
+		
+		public Item next() {
+			if (!hasNext()) throw new NoSuchElementException();
+			Item item = current.item;
+			lastVisited = current;
+			current = current.next;
+			return item;
+		}
+		
+		public Node getLastVisited() { return lastVisited; }
 	}
 	
 	// unit testing (required)
 	public static void main(String[] args) {
-	
+
+		
 	}
 }
